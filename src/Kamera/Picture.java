b829c0616b40085e5014972d5a49bee4888a2217
ilieicheatisteal <eyupy99@gmail.com>
2024-10-23 12:25:47 +0200

    kamera

---
+++ src/Kamera/Picture.java
@@ -0,0 +1,21 @@
+package Kamera;
+
+public class Picture {
+    private int size;
+    private String name;
+    private long creationDate;
+
+    public Picture(int size, String name) {
+        this.size = size;
+        this.name = name;
+        this.creationDate = System.currentTimeMillis();
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
